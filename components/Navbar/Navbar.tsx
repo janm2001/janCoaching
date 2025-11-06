2972b6fa50739ef223db1f5817adb89cbf210211
janm2001 <misterforce1@gmail.com>
2025-11-06 16:15:05 +0100

    feat: init project and adding template for navbar

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Link from "next/link";
+import { UserIcon } from "lucide-react";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Avatar } from "../ui/avatar";
+
+export function Navbar() {
+  const isMobile = useIsMobile();
+  return (
+    <div className="flex items-center justify-between p-4">
+      <Link href="/" className="text-xl font-bold">
+        Jan Coaching
+      </Link>
+      <NavigationMenu viewport={isMobile} className="flex space-between">
+        <NavigationMenuList className="flex-wrap">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/plans">PLANS</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/blog">BLOG</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/about">ABOUT</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <Avatar>
+                <UserIcon />
+              </Avatar>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      
+    </div>
+  );
+}
+
+
